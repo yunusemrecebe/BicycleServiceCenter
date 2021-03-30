49980fc1864d37d879d7773a9d0516f29150dff0
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2021-03-30 22:36:34 +0300

    Framework İçin Core Katmanı Dahil Edildi

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        T Get(Expression<Func<T, bool>> filter);
+        IList<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T Entity);
+        void Update(T Entity);
+        void Delete(T Entity);
+    }
+}
---
+++ Core/Utilities/Messages/AspectMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Messages
+{
+    public static class AspectMessages
+    {
+        public static string WrongValidationType = "Hatalı Validation Tipi";
+        public static string WrongLoggerType = "Hatalı Logger Tipi";
+    }
+}
