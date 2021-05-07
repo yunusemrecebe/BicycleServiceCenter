c424336546cdc4978417ae9bdc79691293bc54c8
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2021-05-07 12:27:52 +0300

    ReportManager iş kuralı eklendi

--- Business/Concrete/ReportManager.cs
+++ Business/Concrete/ReportManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Dtos;
@@ -8,19 +10,37 @@ namespace Business.Concrete
     public class ReportManager : IReportService
     {
         IReportDal _reportDal;
+        IEmployeeService _employeeService;
+        IProductService _productService;
 
-        public ReportManager(IReportDal reportDal)
+        public ReportManager(IReportDal reportDal, IEmployeeService employeeService, IProductService productService)
         {
             _reportDal = reportDal;
+            _employeeService = employeeService;
+            _productService = productService;
         }
 
         public IDataResult<ReportForEmployeeDto> GetReportForEmployee(int employeeId)
         {
+            var result = _employeeService.GetById(employeeId).Data;
+
+            if (result == null)
+            {
+                return new ErrorDataResult<ReportForEmployeeDto>(Messages.IdValueIsInvalid);
+            }
+
             return new SuccessDataResult<ReportForEmployeeDto>(_reportDal.GetReportForEmployee(employeeId));
         }
 
         public IDataResult<ReportForProductDto> GetReportForProduct(int productId)
         {
+            var result = _productService.GetById(productId).Data;
+
+            if (result == null)
+            {
+                return new ErrorDataResult<ReportForProductDto>(Messages.IdValueIsInvalid);
+            }
+
             return new SuccessDataResult<ReportForProductDto>(_reportDal.GetReportForProduct(productId));
         }
     }
